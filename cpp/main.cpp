b4215957a8186b586ea47b59a990e3deb8337cbd
bryanlimanascimento84-eng <bryan.limanascimento84@gmail.com>
2026-04-29 00:31:23 +0000

    Adiciona implementação de verificação de número par ou ímpar, leitura de número racional e cálculo de ASCII

---
+++ cpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+int stop = 0,
+while (stop!=1)
+{
+    int a,b,c;
+    std::cin>>a
+    std::cin>>b
+    std::cout<<"Resultado é "<<a + b <<"Deeja parar? Y / N"<<\n/;
+    std::cin>>c;
+}
+return 0;
